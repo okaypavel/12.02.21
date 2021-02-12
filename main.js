e2950e01ebeeae8d2f9d899bd0d4f0a31b1138e6
Pavlo Chaikivskyi <cajkovskijpavel5@gmail.com>
2021-02-12 15:38:28 +0100

    js

---
+++ main.js
@@ -0,0 +1,106 @@
+// const isDog = true;
+// let imie = "Ala ma kota."
+// imie = "Ala ma psa."
+// console.log("imie", imie)
+
+// const isCat = true;
+// let number = 96
+// number = 69
+// console.log("number", number)
+
+// const num1 = 132
+// const num2 = 312
+// const num3 = 231
+// const wynik = num1 + num2 + num3
+// console.log("wynik",wynik)
+
+// const imie = "Jan"
+// const nazwisko = "Nowak"
+
+// const wynik = imie.toLowerCase() + ' ' + nazwisko.toLowerCase()
+// console.log(wynik)
+
+
+// const tablica = ["Oleg", "Pawel", "Romana", "Roman", "Lera", "Oskar", "Alex", "Marta", "Lew", "Ksenia", "Elena", "Ewa", "Alija", "Lilia", "Diana", "Kuba", "Karolina", "Max", "Fabian", "Leon", "Alan", "Warwara", "Dawid", "Dewid", "Paul", "Lukasz", "Kacper", "Michal", "Wiktor", "Wiktoria"]
+
+// console.log(tablica)
+// console.log("imie numer 0",tablica[0])
+// console.log("imie numer 1",tablica[1])
+// console.log("imie numer 2",tablica[2])
+// console.log("imie numer 3",tablica[3])
+// console.log("imie numer 4",tablica[4])
+// console.log("imie numer 5",tablica[5])
+// console.log("imie numer 6",tablica[6])
+// console.log("imie numer 7",tablica[7])
+// console.log("imie numer 8",tablica[8])
+// console.log("imie numer 9",tablica[9])
+// console.log("imie numer 10",tablica[10])
+// console.log("imie numer 11",tablica[11])
+// console.log("imie numer 12",tablica[12])
+// console.log("imie numer 13",tablica[13])
+// console.log("imie numer 14",tablica[14])
+// console.log("imie numer 14",tablica[14])
+// console.log("imie numer 15",tablica[15])
+// console.log("imie numer 16",tablica[16])
+// console.log("imie numer 17",tablica[17])
+// console.log("imie numer 18",tablica[18])
+// console.log("imie numer 19",tablica[19])
+// console.log("imie numer 20",tablica[20])
+// console.log("imie numer 21",tablica[21])
+// console.log("imie numer 22",tablica[22])
+// console.log("imie numer 23",tablica[23])
+// console.log("imie numer 24",tablica[24])
+// console.log("imie numer 25",tablica[25])
+// console.log("imie numer 26",tablica[26])
+// console.log("imie numer 27",tablica[27])
+// console.log("imie numer 28",tablica[28])
+// console.log("imie numer 29",tablica[29])
+
+// tablica.forEach(item => {
+//     console.log("stare", item)
+//     item = "Henryk"
+//     console.log(item)
+// })
+
+// const tab1 = tablica.map((item, index) => item = "henryk")
+// console.log(tab1)
+// //
+// const pawel = tablica.find(item => {
+//     if(item === "pawel") {
+//         return item
+//     }
+// })
+// console.log(pawel)
+
+// const tablLicz = new Array(1, 2, 69, 885, 777)
+// console.log(tablLicz)
+
+// const wiekszeOdJeden =
+//     tablLicz.some(item => item > 884)
+// console.log(wiekszeOdJeden)
+
+// tablLicz.push(999)
+// console.log(tablLicz)
+
+// const liczbyDo99 = new Array()
+// for (let i = 0; i < 1000; i++) {
+//     liczbyDo99.push(i / 2)
+// }
+// console.log(liczbyDo99)
+
+const liczbyDo199 = new Array()
+for (let i = 0; i < 200; i++) {
+    if (i % 2 !== 0) {
+        liczbyDo199.push(i)
+    }
+}
+console.log(liczbyDo199)
+
+// let indexLiczby88
+// console.log(liczbyDo199)
+// liczbyDo199.forEach((item, index) => {
+//     if(item === 88) {
+//         indexLiczby88 = index
+//     }
+// })
+// console.log(indexLiczby88, liczbyDo199)
